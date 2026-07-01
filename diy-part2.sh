9e32e0d9172a7c78ffbdb717c7e3e03d945297b9
羅莉安的小飛龍 <yuzhuang233@outlook.com>
2026-07-01 22:23:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,19 +10,3 @@ if [ -f "package/kernel/mac80211/files/lib/wifi/mac80211.sh" ]; then
     sed -i 's/hwmode=11g/hwmode=11a/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 fi
 
-# =========================================================
-# 补丁 2：强制开启简体中文支持（确保不被 defconfig 删掉）
-# =========================================================
-echo "CONFIG_LUCI_LANG_zh_Hans=y" >> .config
-echo "CONFIG_PACKAGE_luci-i18n-base-zh-cn=y" >> .config
-echo "CONFIG_PACKAGE_luci-i18n-opkg-zh-cn=y" >> .config
-echo "CONFIG_PACKAGE_luci-i18n-firewall-zh-cn=y" >> .config
-
-# =========================================================
-# 补丁 3：为你想要但经常丢失的核心应用做“保活强制注入”
-# =========================================================
-# 这样即便上面的 git clone 漏了某些依赖，也能保证基础 LuCI 和对应插件被强行编译
-echo "CONFIG_PACKAGE_luci=y" >> .config
-echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> .config
-echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
-echo "CONFIG_PACKAGE_luci-app-passwall=y" >> .config
