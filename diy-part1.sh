1063c1553557c1f90e13f0200027a6b7c2c69ad5
羅莉安的小飛龍 <yuzhuang233@outlook.com>
2026-07-01 22:35:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 # Un-comment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# 注入包含各种流行插件的第三方大仓库源（给编译做双重保险）
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+
+# 配合你的特殊克隆脚本，提升克隆成功率
+git config --global http.lowSpeedLimit 0
+git config --global http.lowSpeedTime 999999
